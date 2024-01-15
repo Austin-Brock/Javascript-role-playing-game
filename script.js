4b1d4cbf1edbecaf6a6185d9050e0e17fb8c64c3
AustinBrockUGA <135121147+Austin-Brock@users.noreply.github.com>
2024-01-15 17:14:17 -0500

    updated js file

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ function buyWeapon() {
     text.innerText = "You now have a new weapon.";
     text.innerText = "You now have a " + newWeapon + ".";
     inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: ";
   }
 }
 
